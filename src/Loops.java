d1bc8343136d15f28469ffe2fa47ef380c38b6bc
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-01-28 08:53:23 -0700

    changes

--- src/Loops.java
+++ src/Loops.java
@@ -1,36 +1,35 @@
 public class Loops {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         int x = 0;
-        while (x <= 100)
-        {
+        while (x <= 100) {
             System.out.println("The value of x is:" + x);
-            x+=2;
+            x += 2;
         }
         int y = 100;
-        while (y >= 0)
-        {
+        while (y >= 0) {
             System.out.println("The value of y is:" + y);
-            y-=3;
+            y -= 3;
         }
 
-        for (int z = 1; z < 101; z+=2) {
+        for (int z = 1; z < 101; z += 2) {
             System.out.println("The value of z is:" + z);
         }
 
         for (int h = 1; h <= 100; h++) {
 
-            if(h % 5 == 0 && h % 3 == 0) {
+            if (h % 5 == 0 && h % 3 == 0) {
                 System.out.println("HelloWorld");
-            } else if(h % 5 == 0) {
+            } else if (h % 5 == 0) {
                 System.out.println("World");
-            } else if(h % 3 == 0) {
+            } else if (h % 3 == 0) {
                 System.out.println("Hello");
             } else {
                 System.out.println("The value of h is:" + h);
             }
 
         }
+
 
     }
 }
